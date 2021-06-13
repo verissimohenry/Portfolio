2dc369b90e4ebd3e72d114df976f9ad2f93a15eb
verissimohenry <verissimohenry04@gmail.com>
2021-06-13 18:11:00 +0100

    add project data js

---
+++ project-data.js
@@ -0,0 +1,15 @@
+export default function ProjectData() {
+    let project_data = { 1: {
+        title: 'Stay-in-Touch',
+        tags: ['HTML/CSS', 'Bootstrap'],
+        image: 'assets/stayintouch.png',
+        description: 'Stay-in-Touch is a app where you can sign up (and/or sign in), create an account to talk to friends and add friends. View profile created by all users and view only signed in user\'s page.',
+        live_link: 'https://peaceful-stream-34920.herokuapp.com/',
+        source_link: 'https://github.com/verissimohenry/Stay-in-Touch.git'
+    },
+    project_2: {
+
+    }
+}
+return project_data;
+}
